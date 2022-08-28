5747bb1a6b66f3d18a891ccf553ee71d5d7d0993
MehediNSU <mehedi.hasan26@northsouth.edu>
2022-08-28 19:43:57 +0600

    added all files

---
+++ src/js/controller.js
@@ -0,0 +1,35 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import recipeView from './views/recipeView.js';
+
+const recipeContainer = document.querySelector('.recipe');
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    //console.log(id);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    //1) Loading recipe
+    await model.loadRecipe(id);
+
+    //2)rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+controlRecipes();
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
+
+//window.addEventListener('hashchange', showRecipe);
