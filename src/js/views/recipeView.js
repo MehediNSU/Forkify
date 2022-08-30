6747f847e143c496321e5fb63cd3f7128190e015
MehediNSU <mehedi.hasan26@northsouth.edu>
2022-08-30 15:40:39 +0600

    implementing search results - part1

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,6 @@
 //import icons from '../img/icons.svg'; //percel 1
 import icons from 'url:../../img/icons.svg'; //percel 2
 import { Fraction } from 'fractional';
-console.log(Fraction);
 export class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
