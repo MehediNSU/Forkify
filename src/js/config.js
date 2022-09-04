2dc4f3d50eab8fe47c587c37b22f65314af93818
MehediNSU <mehedi.hasan26@northsouth.edu>
2022-09-04 16:15:01 +0600

    uploading a new recipe - part2

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10 * 1000;
 export const RES_PER_PAGE = 10;
+export const KEY = '561078f0-f337-40e0-89c8-eb61c7f6f3ad';
+export const MODAL_CLOSE_SEC = 2.5;
